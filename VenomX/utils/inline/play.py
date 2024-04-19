eb057a2752eb6c3493ea9d2e530d45cea848c845
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-04-20 01:40:29 +0530

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -63,18 +63,26 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-            ],
+        ],
+        [
+            InlineKeyboardButton(
+                text="",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
         [
             InlineKeyboardButton(
                 text=_["PL_B_2"],
@@ -90,6 +98,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
             InlineKeyboardButton(text="⛩️ Gʀᴏᴜᴘꨄ︎ ⛩️", url=f"https://t.me/BWF_MUSIC1"),
+        ],
     ]
     return buttons
 
