457cabb80cbdeedbd0eeb17d8647f81ba034447d
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-04-20 01:16:40 +0530

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -1,8 +1,7 @@
 import math
+from config import SUPPORT_CHAT, OWNER_ID
+from pyrogram.types import InlineKeyboardButton
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-import config
 from VenomX.utils.formatters import time_to_seconds
 
 ## After Edits with Timer Bar
