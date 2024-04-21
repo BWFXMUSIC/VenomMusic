6472e52bceab18c3415f04b9a222ea408666f880
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-04-21 22:37:44 +0530

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -77,17 +77,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             ],
         [
             InlineKeyboardButton(text="🥰[↻]🥰", callback_data=f"ADMIN Replay|{chat_id}"),
-            ),
             InlineKeyboardButton(
-                text="✨ 𝐎ωиєя ⏎", url=f"https://t.me/ALL_QUIZ_TAME"
-            ),
-        ],
-        [
-           InlineKeyboardButton(
-                text="💮︎ ᴄᴘᴀɴɴᴇʟ", url=f"https://t.me/ALL_QUIZ_TAME"
-            ),
-           InlineKeyboardButton(
-                text="⛩️ Gʀᴏᴜᴘꨄ︎ ⛩️", url=f"https://t.me/BWF_MUSIC1"
+                text="🥰[sᴜᴘᴘᴏʀᴛ]🥰", url=SUPPORT_CHAT,
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
