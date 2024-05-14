c8b0b5c9d9039d54cbbdefff1b01024229600457
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-05-14 13:35:13 +0530

    Update Repo.py

--- VenomX/plugins/admins/Repo.py
+++ VenomX/plugins/admins/Repo.py
@@ -2,7 +2,7 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from VenomX import app as bot
 import requests
-from config import 
+from config import OWNER_ID
 from VenomX.utils.errors import capture_err
 
 start_txt = """**
