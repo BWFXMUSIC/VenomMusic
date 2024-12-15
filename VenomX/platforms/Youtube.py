4c2e6816604defbecb028cc5593583a6bdeb2757
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-12-15 09:58:11 +0530

    ༄𝐿 2 𝙍.🖤🜲𝐊𝐈𝐍𝐆❦︎ 𝆺𝅥⃝🍷

--- VenomX/platforms/Youtube.py
+++ VenomX/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from TanuMusic.utils.formatters import time_to_seconds
-from TanuMusic.utils.database import is_on_off
+from VenomX.utils.formatters import time_to_seconds
+from VenomX.utils.database import is_on_off
 
 
 def cookies():
